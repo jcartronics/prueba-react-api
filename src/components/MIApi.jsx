51537f641b1e9bb039d249da9d9272a65e1052d4
jcartronics <jcar-95@hotmail.com>
2023-05-21 04:02:37 -0400

    Agrega funcionalidad a Compoennte MiApi

--- src/components/MIApi.jsx
+++ src/components/MIApi.jsx
@@ -1,8 +1,66 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useState } from 'react'
 
 const MIApi = () => {
+const [indicadores, setIndicadores] = useState([])
+const [periodo, setPeriodo] = useState('2023')
+
+useEffect(() => {
+    getIndicadores()
+    console.log(indicadores)
+}, [periodo]);
+
+const getIndicadores = async () => {
+    try {
+        console.log(`El periodo seleccionado es ${periodo}`)
+        const url = `https://mindicador.cl/api/dolar/${periodo}`
+        const response = await fetch(url)
+        const data = await response.json()
+        setIndicadores(data)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const handleForm = (e) => {
+    e.preventDefault()
+    setPeriodo(e.target.periodo.value)
+}
+
   return (
-    <div>MIApi</div>
+    <div>
+        <form onSubmit={handleForm}>
+            <select name="periodo">
+                <option value="2023">2023</option>
+                <option value="2022">2022</option>
+                <option value="2021">2021</option>
+                <option value="2020">2020</option>
+            </select>
+            <input type="submit" value="Enviar" />
+        </form>
+
+        <table>
+            <thead>
+                <tr>
+                    <th>Fecha</th>
+                    <th>Valor</th>
+                </tr>
+            </thead>
+            <tbody>
+                {indicadores.serie &&
+                        indicadores.serie.map((indicador, index) => {
+                            const fecha = new Date(indicador.fecha)
+                            const fechaFormateada = fecha.toLocaleDateString()
+
+                            return (
+                                <tr key={index}>
+                        <td>{fechaFormateada}</td>
+                        <td>{indicador.valor}</td>
+                    </tr>
+                )})}
+            </tbody>
+        </table>
+    </div>
   )
 }
 
